e4619b05e8d01d09396f9a44a87012d099906b95
artiow <artem.namednev@gmail.com>
2023-09-02 16:33:41 +0400

    custom error handler and concurrency support added

--- kafka/src/main/java/artiow/examples/kafka/ClockDemoProducer.java
+++ kafka/src/main/java/artiow/examples/kafka/ClockDemoProducer.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -32,8 +33,22 @@ public class ClockDemoProducer {
             .thenAccept(sendResult -> producedUuids.add(sendResult.getProducerRecord().key()));
     }
 
-    @KafkaListener(topics = {"kafka-topic-example"})
-    public void consumeTick(ConsumerRecord<UUID, DemoData> rec) {
+    @KafkaListener(
+        topicPartitions = {@TopicPartition(topic = "kafka-topic-example", partitions = "0,2")},
+        concurrency = "2")
+    public void consumeTick_0(ConsumerRecord<UUID, DemoData> rec) {
+        consumeTick(0, rec);
+    }
+
+    @KafkaListener(
+        topicPartitions = {@TopicPartition(topic = "kafka-topic-example", partitions = "1,3")},
+        concurrency = "2")
+    public void consumeTick_1(ConsumerRecord<UUID, DemoData> rec) {
+        consumeTick(1, rec);
+        throw new RuntimeException(rec.key().toString());
+    }
+
+    private void consumeTick(int id, ConsumerRecord<UUID, DemoData> rec) {
         final var uuid = Objects.requireNonNull(rec.key(), "Key must be non null");
         final var data = Objects.requireNonNull(rec.value(), "Value must be non null");
 
@@ -50,15 +65,17 @@ public class ClockDemoProducer {
             ((int) (rec.timestamp() % 1000)) * 1_000_000,
             ZoneOffset.UTC);
         if (!producedUuids.contains(uuid)) {
-            log.warn("""
-                Unknown key: {} (offset: {}, timestamp: {}, value: {})""",
+            log.warn(
+                "[consumer-{}] Unknown key: {} (offset: {}, timestamp: {}, value: {})",
+                id,
                 uuid,
                 offset,
                 timestamp,
                 data);
         } else {
-            log.info("""
-                Consumed key: {} (offset: {}, timestamp: {}, value: {})""",
+            log.info(
+                "[consumer-{}] Consumed key: {} (offset: {}, timestamp: {}, value: {})",
+                id,
                 uuid,
                 offset,
                 timestamp,
--- kafka/src/main/java/artiow/examples/kafka/KafkaConfig.java
+++ kafka/src/main/java/artiow/examples/kafka/KafkaConfig.java
@@ -1,15 +1,43 @@
 package artiow.examples.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
     public NewTopic topic() {
-        return TopicBuilder.name("kafka-topic-example").build();
+        return TopicBuilder
+            .name("kafka-topic-example")
+            .partitions(4)
+            .build();
+    }
+
+    @Bean
+    public CommonErrorHandler errorHandler() {
+        final var log = LoggerFactory.getLogger(getClass() + ".errorHandler");
+        return new DefaultErrorHandler((record, exception) -> {
+            if (exception instanceof ListenerExecutionFailedException listenerException) {
+                final var originException = listenerException.getCause();
+                log.error(
+                    "Record {} skipped because of an exception {} caused by {}",
+                    record.toString(),
+                    listenerException,
+                    originException.toString());
+            } else {
+                log.error(
+                    "Record {} skipped because of an exception {}",
+                    record.toString(),
+                    exception.toString());
+            }
+        }, new FixedBackOff(0, 0));
     }
 }
